9e0368faa6be70b9297b396ca0198a456f448df6
Alex <abecker@covenantfrazer.org>
2020-08-28 15:54:00 -0400

    Transitions working successfully. I'll be going with a functional style, preferring functional components whenever possible to keep things stateless. The only state I'd like to refer to is the data kept in the database, nothing programmatic. Working on spreading out functions into /components to make things easier to jump around in. I also started the {h} helper object that will keep all my reusable functions in it.

--- src/App.js
+++ src/App.js
@@ -1,79 +1,55 @@
 import React from "react";
-import ReactDOM from "react-dom";
+//import ReactDOM from "react-dom";
 import "./App.css";
 
-class Cloud extends React.Component {
-  render() {
-    return <div id={this.props.id} className="cloud backgroundImg"></div>;
-  }
-  componentDidMount() {
-    const el = ReactDOM.findDOMNode(this);
-    setTimeout(function () {
-      el.classList.add("cloudMove");
-    }, 100);
-  }
-}
+//Components
+import Header from "./components/Header";
 
-function App() {
+//Helpers
+import h from "./helpers/h";
+
+/*Example Functional Component*/
+/* Const because the function doesn't change. It accepts all properties in the JSX    */
+const Cloud = (props) => {
+  /* You can include whatever functions you'd like within the component as normal       */
+  const startClouds = function () {
+    setTimeout(function () {
+      const clouds = h.findClass("cloud");
+      for (var i = 0; i < clouds.length; i++) {
+        clouds[i].classList.add("cloudMove");
+      }
+    }, 100);
+  };
+  /* You can also add event listeners as usual */
+  window.addEventListener("onload", startClouds());
+  /* Just return JSX to render. I think you can enclose multiple elements in a fragment? */
+  return <div id={props.id} className="cloud backgroundImg"></div>;
+};
+
+const MainButton = (props) => {
   return (
-    <div className="App">
-      <div className="view" id="homeView">
-        <div id="churchImg" className="backgroundImg"></div>
-        <Cloud id="cloudA"></Cloud>
-        <Cloud id="cloudB"></Cloud>
-        <div className="header" id="homeHeader">
-          <div id="mainTitle">LovingCare</div>
-          <div className="menuIcon icon">
-            <ion-icon name="menu"></ion-icon>
-          </div>
-        </div>
-        <div id="mainButtons">
-          <div className="mainIcon icon" id="logBookIcon">
-            <ion-icon name="book-outline"></ion-icon>
-            <span>Logbook</span>
-          </div>
-          <div className="mainIcon icon" id="directoryIcon">
-            <ion-icon name="people"></ion-icon>
-            <span>Directory</span>
-          </div>
-          <div className="mainIcon icon" id="newNoteIcon">
-            <ion-icon name="add-outline"></ion-icon>
-            <span>Add New Note</span>
-          </div>
-          <div className="mainIcon icon" id="newParIcon">
-            <ion-icon name="person-add"></ion-icon>
-            <span>New Parishioner</span>
-          </div>
-        </div>
-      </div>
-      <div className="view off" id="logbookView">
-        <div className="header" id="logbookHeader">
-          <div id="logbookTitle">
-            <span>Logbook</span>
-          </div>
-          <div className="logbookIcon icon" id="filterIcon"></div>
-          <div className="menuIcon icon"></div>
-        </div>
-        <div id="logbook"></div>
-      </div>
-      <div className="view off" id="directoryView">
-        <div className="header" id="directoryHeader">
-          <div id="directoryTitle">
-            <span>Directory</span>
-          </div>
-          <div className="menuIcon icon"></div>
-        </div>
-        <div id="directory"></div>
-      </div>
-      <div className="view off" id="noteView">
-        <div className="header" id="noteHeader">
-          <div id="noteTitle">New Note</div>
-          <div className="saveIcon icon" id="noteSave"></div>
-          <div className="menuIcon icon"></div>
-        </div>
-        <div id="noteEditor"></div>
-        <div className="textGroup" id="noteTextGroup">
-          <div className="textInput" id="noteParText">
+    <div
+      className="mainButton icon"
+      id={props.id}
+      onClick={(e) => h.openView(e, props.open, props.close)}
+    >
+      <ion-icon name={props.ionIcon}></ion-icon>
+      <span>{props.title}</span>
+    </div>
+  );
+};
+
+const NoteEditor = (props) => {
+  return (
+    <div className={"noteEditor " + props.className}>
+      <Header
+        id={"noteHeader" + props.id}
+        title={props.title}
+        icon="save-outline"
+      ></Header>
+      <div className="viewBody">
+        <div className="textGroup" id={"noteTextGroup" + props.id}>
+          <div className="textInput" id={"noteParText" + props.id}>
             <ion-icon name="person"></ion-icon>
             <label htmlFor="parNameInput" className="off">
               Attach to Parishioner
@@ -83,73 +59,86 @@ function App() {
               type="text"
               autoComplete="off"
               defaultValue="Attach to Parishioner"
-              id="parNameInput"
+              id={"parNameInput" + props.id}
             />
-            <div className="autocomplete" id="parAutocomplete"></div>
+            <div
+              className="autocomplete"
+              id={"parAutocomplete" + props.id}
+            ></div>
           </div>
-          <label htmlFor="tagInput" className="off">
+          <label htmlFor={"tagInput" + props.id} className="off">
             Tags
           </label>
-          <div className="textInput" id="noteTagText">
-            <ion-icon name="category"></ion-icon>
+          <div className="textInput" id={"noteTagText" + props.id}>
+            <ion-icon name="pricetags-outline"></ion-icon>
             <input
               className="noteInput"
               type="text"
               autoComplete="off"
               defaultValue="Tags"
-              id="tagInput"
+              id={"tagInput" + props.id}
             />
-            <div className="autocomplete" id="catAutocomplete"></div>
+            <div
+              className="autocomplete"
+              id={"catAutocomplete" + props.id}
+            ></div>
           </div>
           <label htmlFor="noteDateInput" className="off">
             Date
           </label>
-          <div className="textInput" id="noteDateText">
+          <div className="textInput" id={"noteDateText" + props.id}>
             <ion-icon name="calendar"></ion-icon>
             <input
               className="noteInput"
               type="text"
               autoComplete="off"
               defaultValue="Date"
-              id="noteDateInput"
+              id={"noteDateInput" + props.id}
             />
           </div>
         </div>
-        <div id="noteCheckGroup">
-          <div id="emailSelf">
-            <input type="checkbox" id="emailSelfCB" />
+        <div id={"noteCheckGroup" + props.id}>
+          <div id={"emailSelf" + props.id}>
+            <input type="checkbox" id={"emailSelfCB" + props.id} />
             <label htmlFor="emailSelfCB">Email to Myself</label>
           </div>
-          <div id="prayerRequest">
-            <input type="checkbox" id="prayerRequestCB" />
+          <div id={"prayerRequest" + props.id}>
+            <input type="checkbox" id={"prayerRequestCB" + props.id} />
             <label htmlFor="prayerRequestCB">Send as prayer request</label>
           </div>
-          <div id="nextDay">
-            <input type="checkbox" id="nextDayCB" />
+          <div id={"nextDay" + props.id}>
+            <input type="checkbox" id={"nextDayCB" + props.id} />
             <label htmlFor="nextDayCB">Next day reminder</label>
           </div>
-          <div id="nextMonday">
-            <input type="checkbox" id="nextMondayCB" />
+          <div id={"nextMonday" + props.id}>
+            <input type="checkbox" id={"nextMondayCB" + props.id} />
             <label htmlFor="nextMondayCB">Next Monday reminder</label>
           </div>
-          <div id="interaction">
-            <input type="checkbox" id="interactionCB" />
+          <div id={"interaction" + props.id}>
+            <input type="checkbox" id={"interactionCB" + props.id} />
             <label htmlFor="interactionCB">Log as interaction</label>
           </div>
-          <div id="addToBio">
-            <input type="checkbox" id="addToBioCB" />
+          <div id={"addToBio" + props.id}>
+            <input type="checkbox" id={"addToBioCB" + props.id} />
             <label htmlFor="addToBioCB">Add to bio</label>
           </div>
         </div>
       </div>
-      <div className="view off" id="parView">
-        <div className="header" id="parHeader">
-          <div id="parTitle">New Parishioner</div>
-          <div className="saveIcon icon" id="parSave"></div>
-          <div className="menuIcon icon"></div>
-        </div>
-        <div className="textGroup" id="parTextGroup">
-          <div className="textInput" id="parParText">
+    </div>
+  );
+};
+
+const ParEditor = (props) => {
+  return (
+    <div className="parEditor">
+      <Header
+        id={"parHeader" + props.id}
+        title="New Parishioner"
+        icon="save-outline"
+      ></Header>
+      <div className="viewBody">
+        <div className="textGroup" id={"parTextGroup" + props.id}>
+          <div className="textInput" id={"parParText" + props.id}>
             <ion-icon name="person"></ion-icon>
             <label htmlFor="parInput" className="off">
               Name
@@ -159,11 +148,11 @@ function App() {
               type="text"
               autoComplete="off"
               defaultValue="Name"
-              id="parInput"
+              id={"parInput" + props.id}
             />
           </div>
-          <div className="textInput" id="noteTagText">
-            <ion-icon name="calendar"></ion-icon>
+          <div className="textInput" id={"noteTagText" + props.id}>
+            <ion-icon name="gift-outline"></ion-icon>
             <label htmlFor="birthdayInput" className="off">
               Birthday
             </label>
@@ -172,11 +161,11 @@ function App() {
               type="text"
               autoComplete="off"
               defaultValue="Birthday"
-              id="birthdayInput"
+              id={"birthdayInput" + props.id}
             />
           </div>
-          <div className="textInput" id="noteDateText">
-            <ion-icon name="calendar"></ion-icon>
+          <div className="textInput" id={"noteDateText" + props.id}>
+            <ion-icon name="rose-outline"></ion-icon>
             <label htmlFor="anniversaryInput" className="off">
               Anniversary
             </label>
@@ -185,11 +174,11 @@ function App() {
               type="text"
               autoComplete="off"
               defaultValue="Anniversary"
-              id="anniversaryInput"
+              id={"anniversaryInput" + props.id}
             />
           </div>
-          <div className="textInput" id="noteDateText">
-            <ion-icon name="person"></ion-icon>
+          <div className="textInput" id={"noteDateText" + props.id}>
+            <ion-icon name="briefcase-outline"></ion-icon>
             <label htmlFor="occupationInput" className="off">
               Occupation
             </label>
@@ -198,11 +187,11 @@ function App() {
               type="text"
               autoComplete="off"
               defaultValue="Occupation"
-              id="occupationInput"
+              id={"occupationInput" + props.id}
             />
           </div>
-          <div className="textInput" id="noteDateText">
-            <ion-icon name="category"></ion-icon>
+          <div className="textInput" id={"noteDateText" + props.id}>
+            <ion-icon name="git-merge-outline"></ion-icon>
             <label htmlFor="relationshipsInput" className="off">
               Relationships
             </label>
@@ -211,11 +200,11 @@ function App() {
               type="text"
               autoComplete="off"
               defaultValue="Relationships"
-              id="relationshipsInput"
+              id={"relationshipsInput" + props.id}
             />
           </div>
-          <div className="textInput" id="noteDateText">
-            <ion-icon name="category"></ion-icon>
+          <div className="textInput" id={"noteDateText" + props.id}>
+            <ion-icon name="color-filter-outline"></ion-icon>
             <label htmlFor="groupsInput" className="off">
               Groups
             </label>
@@ -224,18 +213,108 @@ function App() {
               type="text"
               autoComplete="off"
               defaultValue="Groups"
-              id="groupsInput"
+              id={"groupsInput" + props.id}
             />
           </div>
         </div>
-        <div id="parNotes">
-          <div id="parNoteHeader">
-            <div id="addParNote">
-              <ion-icon name="plus"></ion-icon>
+        <div id={"parNotes" + props.id}>
+          <div id={"parNoteHeader" + props.id}>
+            <div id={"addParNote" + props.id}>
+              <ion-icon name="add-outline"></ion-icon>
             </div>
-            <div id="parNoteTitle">Notes</div>
+            <div id={"parNoteTitle" + props.id}>Notes</div>
           </div>
         </div>
+      </div>
+    </div>
+  );
+};
+
+const Log = (props) => {
+  return (
+    <div className="log">
+      <div className="logHeading">Log Heading</div>
+      <div className="logPreview">
+        This is a preview of the log entry. Many interesting things will be
+        included here
+      </div>
+      <NoteEditor
+        className="off"
+        title="Note about something important"
+        id={props.id}
+      />
+    </div>
+  );
+};
+
+function App() {
+  return (
+    <div className="App">
+      <div className="view" id="homeView">
+        <div id="churchImg" className="backgroundImg"></div>
+        <Cloud id="cloudA"></Cloud>
+        <Cloud id="cloudB"></Cloud>
+        <Header id="homeHeader" title="LovingCare"></Header>
+
+        <div id="mainButtons">
+          <MainButton
+            id="logBookIcon"
+            ionIcon="book-outline"
+            title="Logbook"
+            open="logbookView"
+            close="homeView"
+          />
+          <MainButton
+            id="directoryIcon"
+            ionIcon="people"
+            title="Directory"
+            open="directoryView"
+            close="homeView"
+          />
+          <MainButton
+            id="newNoteIcon"
+            ionIcon="add-outline"
+            title="Add New Note"
+            open="noteView"
+            close="homeView"
+          />
+          <MainButton
+            id="newParIcon"
+            ionIcon="person-add"
+            title="New Parishioner"
+            open="parView"
+            close="homeView"
+          />
+        </div>
+      </div>
+      <div className="view off" id="logbookView">
+        <Header
+          id="logbookHeader"
+          title="Logbook"
+          icon="funnel-outline"
+        ></Header>
+        <div className="viewBody">
+          <div id="logbook">
+            <Log id="1" />
+            <Log id="2" />
+            <Log id="3" />
+            <Log id="4" />
+            <Log id="5" />
+            <Log id="6" />
+          </div>
+        </div>
+      </div>
+      <div className="view off" id="directoryView">
+        <Header id="directoryHeader" title="Directory"></Header>
+        <div className="viewBody">
+          <div id="directory">Directory Entries</div>
+        </div>
+      </div>
+      <div className="view off" id="noteView">
+        <NoteEditor title="New Note"></NoteEditor>
+      </div>
+      <div className="view off" id="parView">
+        <ParEditor title="New Parishioner"></ParEditor>
       </div>
     </div>
   );
