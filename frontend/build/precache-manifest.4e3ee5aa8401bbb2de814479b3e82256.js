e1b544aebe22c650f29f72e165a8fdfe23b51365
Alex <abecker@covenantfrazer.org>
2020-08-30 11:15:01 -0400

    Reorganized into frontend and backend

---
+++ frontend/build/precache-manifest.4e3ee5aa8401bbb2de814479b3e82256.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f9ed9883d1191ebef2fe810620cdc77a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "45a723a779817803e685",
+    "url": "/static/css/main.c1dd09f9.chunk.css"
+  },
+  {
+    "revision": "73199796a86bb383193f",
+    "url": "/static/js/2.d683e2a4.chunk.js"
+  },
+  {
+    "revision": "a2475def2af61acc36893259e4803dfe",
+    "url": "/static/js/2.d683e2a4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "45a723a779817803e685",
+    "url": "/static/js/main.4150ae26.chunk.js"
+  },
+  {
+    "revision": "95706a583cf4c7d42452",
+    "url": "/static/js/runtime-main.7401318a.js"
+  },
+  {
+    "revision": "4ef50592382d8679ca3b7aedb870c45a",
+    "url": "/static/media/church.4ef50592.svg"
+  },
+  {
+    "revision": "60ec37a605a445aae0b6f812d2cff490",
+    "url": "/static/media/church_logo.60ec37a6.svg"
+  },
+  {
+    "revision": "aa90f8d24f29a92676e1355f564a7062",
+    "url": "/static/media/cloud.aa90f8d2.svg"
+  }
+]);
