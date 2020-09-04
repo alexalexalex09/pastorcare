a0c04ec3d8a2837d90133644f12f9740ffe49ad0
Alex <abecker@covenantfrazer.org>
2020-09-04 08:31:08 -0400

    people/add is working

--- backend/routes/people.js
+++ backend/routes/people.js
@@ -20,8 +20,55 @@ mongoose.Promise = global.Promise;
 var db = mongoose.connection;
 db.on("error", console.error.bind(console, "MongoDB connection error:"));
 
+/**
+ *
+ *
+ * @param {*} item add this to array if exists
+ * @param {object} curObj the object to which <item> should be added
+ * @param {string} key the property under which <item> should be added
+ * @returns modified object with or without new key
+ */
+function checkAndAdd(item, curObj, field) {
+  //console.log(field, " item: ", item);
+  if (item) {
+    if (Array.isArray(item)) {
+      curObj = checkAndAddArray(item, curObj, field);
+    } else {
+      curObj[field] = item;
+    }
+    //console.log(field, " update: ", curObj[field]);
+  }
+  //console.log("returning: ", curObj);
+  return curObj;
+}
+
+/**
+ *
+ *
+ * @param {*} item add this to array if exists
+ * @param {object} curObj the object to which <item> should be added
+ * @param {string} field the property under which <item> should be added
+ * @returns modified object with or without new key
+ */
+function checkAndAddArray(item, curObj, field) {
+  //console.log("Arrayitem: ", item);
+  if (item) {
+    if (!curObj[field]) {
+      curObj[field] = [];
+    }
+    for (let key in item) {
+      if (item.hasOwnProperty(key)) {
+        curObj[field].push(item[key]);
+      }
+    }
+    //console.log("Arrayupdate: ", curObj[field]);
+  }
+  //console.log("Arrayreturning: ", curObj);
+  return curObj;
+}
+
 /* Get a person by name */
-router.post("/name", function (req, res) {
+router.post("/get", function (req, res) {
   Person.findOne({ name: req.body.name }).exec(function (err, curUser) {
     if (err) {
       res.send({ err: err });
@@ -31,51 +78,19 @@ router.post("/name", function (req, res) {
   });
 });
 
-/**
- *
- *
- * @param {*} item add this to array if exists
- * @param {object} curObj the object to which <item> should be added
- * @param {string} key the property under which <item> should be added
- * @returns modified object with or without new key
- */
-function checkAndAdd(item, curObj, key) {
-  console.log("item: ", item);
-  if (item) {
-    curObj[key] = item;
-    console.log("update: ", curObj[key]);
-  }
-  console.log("returning: ", curObj);
-  return curObj;
-}
-
 router.post("/add", function (req, res) {
   console.log("body", req.body);
-  var person = {};
-  if (req.body.firstName) {
-    person.firstName = req.body.firstName;
-  }
+  let person = {};
   person = checkAndAdd(req.body.firstName, person, "firstName");
   person = checkAndAdd(req.body.lastName, person, "lastName");
+  person = checkAndAdd(req.body.formalName, person, "formalName");
   person = checkAndAdd(req.body.maidenName, person, "maidenName");
   person = checkAndAdd(req.body.birthday, person, "birthday");
   person = checkAndAdd(req.body.anniversary, person, "anniversary");
-  person.occupations = [];
-  person.groups = [];
-  if (req.body.occupations) {
-    for (let key in req.body.occupations) {
-      if (req.body.occupations.hasOwnProperty(key)) {
-        person.occupations.push(req.body.occupations[key]);
-      }
-    }
-  }
-  if (req.body.groups) {
-    for (let key in req.body.groups) {
-      if (req.body.groups.hasOwnProperty(key)) {
-        person.groups.push(req.body.groups[key]);
-      }
-    }
-  }
+  person = checkAndAdd(req.body.occupations, person, "occupations");
+  person = checkAndAdd(req.body.relationships, person, "relationships");
+  person = checkAndAdd(req.body.groups, person, "groups");
+  console.log("person:", person);
   Person.findOneAndUpdate({ name: req.body.name }, person).exec(function (
     err,
     curUser
